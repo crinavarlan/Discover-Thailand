d2f63552a9c223d4009f35fa6814d3e96a92cc30
Crina Varlan <crina.varlan@yahoo.com>
2018-01-27 18:45:44 +0000

    first commit

---
+++ discover_thailand/urls.py
@@ -0,0 +1,38 @@
+"""discover_thailand URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf.urls import url, include
+from accounts import views as accounts_views
+from home import views as home_views
+from contact import views as contact_views
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+
+    # home url
+    url(r'^$', home_views.get_home, name='home'),
+
+    # authentication url
+    url(r'^register/$', accounts_views.register, name='register'),
+    url(r'^profile/$', accounts_views.profile, name='profile'),
+    url(r'^login/$', accounts_views.login, name='login'),
+    url(r'^logout/$', accounts_views.logout, name='logout'),
+
+    # contact url
+    url(r'^contact/$', contact_views.contact, name='contact'),
+
+]
