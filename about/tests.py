d131bf00c940f66aad8e9ec7cf1e065380c61300
Crina Varlan <crina.varlan@yahoo.com>
2018-02-04 22:59:37 +0000

    tests for accounts added

--- about/tests.py
+++ about/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from about.views import about_map
+from django.core.urlresolvers import resolve
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_about_page(self):
+        about_page = resolve('/about/')
+        self.assertEqual(about_page.func, about_map)
+
+    def test_home_page_status_code_is_not_404(self):
+        home_page = self.client.get('/')
+        self.assertNotEquals(home_page.status_code, 404)
+
+    def test_home_page_status_code_is_not_500(self):
+        home_page = self.client.get('/')
+        self.assertNotEquals(home_page.status_code, 500)
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,64 @@
+from .forms import UserRegistrationForm
 from django.test import TestCase
+from django.conf import settings
+from django import forms
 
-# Create your tests here.
+
+class CustomUserTest(TestCase):
+    def test_registration_form(self):
+        form = UserRegistrationForm({
+            'email': 'tester@test.com',
+            'password1': 'test123',
+            'password2': 'test123',
+            'stripe_id': settings.STRIPE_SECRET,
+            'credit_card_number': 4242424242424242,
+            'cvv': 123,
+            'expiry_month': 9,
+            'expiry_year': 2033,
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_registration_form_fails_with_missing_email(self):
+        form = UserRegistrationForm({
+            'password1': 'Iwillenter_1',
+            'password2': 'Iwillenter_1',
+            'stripe_id': settings.STRIPE_SECRET,
+            'credit_card_number': 4242424242424242,
+            'cvv': 123,
+            'expiry_month': 1,
+            'expiry_year': 2033
+        })
+        self.assertFalse(form.is_valid())
+        self.assertRaisesMessage(forms.ValidationError,
+                                 "Please enter your email address",
+                                 form.full_clean())
+
+    def test_registration_fails_with_empty_password1(self):
+        form = UserRegistrationForm({
+            'email': 'professional_tester@test.com',
+            'password2': 'Iwillenter_1',
+            'stripe_id': settings.STRIPE_SECRET,
+            'credit_card_number': 4242424242424242,
+            'cvv': 123,
+            'expiry_month': 1,
+            'expiry_year': 2033
+        })
+        self.assertFalse(form.is_valid())
+        self.assertRaisesMessage(forms.ValidationError,
+                                 "Passwords do not match",
+                                 form.full_clean())
+
+    def test_registration_fails_with_empty_password2(self):
+        form = UserRegistrationForm({
+            'email': 'professional_tester@test.com',
+            'password1': 'Iwillenter_1',
+            'stripe_id': settings.STRIPE_SECRET,
+            'credit_card_number': 4242424242424242,
+            'cvv': 123,
+            'expiry_month': 1,
+            'expiry_year': 2033
+        })
+        self.assertFalse(form.is_valid())
+        self.assertRaisesMessage(forms.ValidationError,
+                                 "Passwords do not match",
+                                 form.full_clean())
